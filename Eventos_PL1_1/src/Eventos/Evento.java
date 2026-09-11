76a76fc178423f13be4554a37a58a52fbbb7440f
AStojo <antonio.tojo.santos@gmail.com>
2026-09-11 20:34:22 +0100

    Adicção do codigo de Evento
    Correção de problema de commit sem codigo.

--- Eventos_PL1_1/src/Eventos/Evento.java
+++ Eventos_PL1_1/src/Eventos/Evento.java
@@ -1,5 +1,24 @@
 package Eventos;
 
 public class Evento {
+		private String titulo;
+		private String data;
+		private String local;
+		private int totalM;
+		
+		public Evento(String titulo, String data, String local, int totalM) {
+			this.titulo = titulo;
+			this.data = data;
+			this.local = local;
+			this.totalM = totalM;
+		}
 
+		public String getTitulo() {
+			return titulo;
+		}
+		
+		public int getTotalM() {
+			return totalM;
+		}
+		
 }
